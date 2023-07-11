751f59607f8e564a943dca93d687c9a55bf63c0f
Nathan Trautenberg <Natester123@mac.com>
2023-07-10 20:28:57 -0700

    Add reflection and spicy challenge

--- code_challenges/spicy.rb
+++ code_challenges/spicy.rb
@@ -1,6 +1,19 @@
 ## Spicy
 
-Write a Ruby program that prints out a String or Integer: The printed value will depend on the value of an Integer.
-If the Integer is a multiple of 3, print "Fizz". If the Integer is a multiple of 5, print "Buzz".
-If the Integer is a multiple of both 3 and 5, print "FizzBuzz".
-If the Integer is not a multiple of either, print the Integer itself.
+#Write a Ruby program that prints out a String or Integer: The printed value will depend on the value of an Integer.
+#If the Integer is a multiple of 3, print "Fizz".
+#If the Integer is a multiple of 5, print "Buzz".
+#If the Integer is a multiple of both 3 and 5, print "FizzBuzz".
+#If the Integer is not a multiple of either, print the Integer itself.
+
+blt = 1
+
+if blt % 3 == 0 && blt % 5 == 0
+    p "FizzBuzz"
+elsif blt % 3 == 0 
+    p "Fizz"
+elsif blt % 5 == 0
+    p "Buzz"
+else 
+    p blt
+end
--- self_eval.md
+++ self_eval.md
@@ -1,5 +1,8 @@
 # Self Evaluation
 
 1. When is it useful to use a `conditional` in your code?
+    # When you have multiple statements you want to evaluate, or when you want to compare data types
 1. If you could do this assignment over, what would you do differently?
+    # nothing
 1. What about your thinking, learning, or work today brought you the most satisfaction? Why?
+    # recalling modal logic from college
